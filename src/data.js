1d79ec5e3d950681d210df0eb5e22921542eb81f
Hana Čopáková <sandracopakova@gmail.com>
2022-02-04 17:19:03 +0100

    fix build

--- src/data.js
+++ src/data.js
@@ -1,19 +1,6 @@
-export const data = [
+const data = [
   {
     id: 1,
-    body: `### This just in!
-
-Markdown is an *awesome* way to write **very basic HTML** by typing manually
-
-It can be used for:
-
-1. README files
-1. Slack and Discord messages
-1. **So much more!**
-
----
-
-Here's a division 👆`,
   },
   { id: 2, body: `a` },
   { id: 3, body: `b` },
@@ -21,3 +8,5 @@ Here's a division 👆`,
   { id: 5, body: `d` },
   { id: 6, body: `e` },
 ];
+
+export default data;
